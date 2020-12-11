686623ebdcdc103f6f780b3e52fddc98afcdfabe
Katerina Fedorets <katia.fedorets@gmail.com>
2020-12-11 16:16:04 +0200

    servisApi
    добовляет функции для  HTTP запросов

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import "./sass/main.scss";
-
-
-
+import './sass/main.scss';
+import './js/serviceApi';
 // import "./js/open-modal-search";
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/Material.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import { defaults, alert } from '@pnotify/core';
+import axios from 'axios';
+
+
+defaults.styling = 'material';
+defaults.icons = 'material';
+
