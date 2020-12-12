9ed84bcd78f435e3a0d0966b5ea505dfa562eab3
abakarovmaks <abakarovmaks@gmail.com>
2020-12-12 18:54:07 +0200

    Убирает с кнопки меню display:none и добавляет выезжающему меню z-index: 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
 import './js/open-modal-authorization';
 import './js/serviceApi';
-import './js/header';
+import './js/openMenuHeader';
 
 // import './js/registerUser';
 // import "./js/open-modal-search";
