7b65573c314d666d4bb0422aaeb905c47af2a5da
Katerina Fedorets <katia.fedorets@gmail.com>
2020-12-12 17:38:41 +0200

    registr
    подключает  модалку регистрации пользователя и функционал регистрации

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./sass/main.scss";
 import "./js/open-modal-authorization";
 import './js/serviceApi';
 // import './js/registerUser';
-// import "./js/open-modal-search";
+import "./js/open-modal-search";
 
 
 
