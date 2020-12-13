ac3faca141ab8350c2abeac77bacdb9f2f7ceb83
sergei-nikitin <65117368+sergei-nikitin@users.noreply.github.com>
2020-12-13 14:10:20 +0200

    добавил события к модалке авторизации
    закрытие при нажатии на esc и клике по backdrop

--- src/js/open-modal-authorization.js
+++ src/js/open-modal-authorization.js
@@ -1,40 +1,22 @@
 const openModalAuthorization = document.querySelector('.login-button-tablet');
 const closeModalAuthorization = document.querySelector('.authorization-button-close');
-const modalAuthorization = document.querySelector("[data-modal]")
+const modalAuthorization = document.querySelector("[data-modal]");
+const backdrop = document.querySelector('.backdrop-authorization');
 
 openModalAuthorization.addEventListener('click', toggleModal);
 closeModalAuthorization.addEventListener('click', toggleModal);
+window.addEventListener('keypress', toggleModal);
+backdrop.addEventListener('click', onBackdropClick);
 
-  function toggleModal() {
+
+function toggleModal() {
     document.body.classList.toggle("modal-open");
     modalAuthorization.classList.toggle("is-hidden");
   }
 
-function onOpenModalAuthorization () {
-    modalAuthorization.classList.remove('.is-hidden');
-    // console.log('hi');
+function onBackdropClick(e) {
+  if (e.target === e.currentTarget) {
+      toggleModal()
+    }
+  }
 
-}
-
-function onCloseModalAuthorization() {
-    modalAuthorization.classList.add('.is-hidden');
- 
-}
-
-
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('.login-button-tablet'),
-//     closeModalBtn: document.querySelector("[data-modal-close-authModal]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     document.body.classList.toggle("backdrop-authorization");
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
