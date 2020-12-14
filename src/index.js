269a7460e3d02fc54250d061898fa63296570013
abakarovmaks <abakarovmaks@gmail.com>
2020-12-14 20:20:01 +0200

    Исправляет выезжающее меню

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import './js/open-modal-authorization';
 import './js/serviceApi';
 import './js/openMenuHeader';
 import './js/hiddenNavSubmenu.js';
-import './js/smoothieSkrollToAnchors.js';
+// import './js/smoothieSkrollToAnchors.js';
 
 import './js/registerUser';
 import './js/open-modal-search.js';
 import './js/open-modal-create-ads';
 
 // import './js/fetch-categories-modal';
-
