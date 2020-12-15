8101fd6621ba049a5faf7fe021d4375e28e287fd
abakarovmaks <abakarovmaks@gmail.com>
2020-12-15 15:23:07 +0200

    Добавляет смену цвета при ховере и фокусе на нужных элементах

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './sass/main.scss';
 import './js/open-modal-authorization';
 import './js/serviceApi';
-import './js/header/buttonsRegisterAccountToggle.js';
 import './js/header/openMenuHeader.js';
 import './js/header/hiddenNavSubmenu.js';
+import './js/header/buttonsRegisterAccountToggle.js';
 // import './js/smoothieSkrollToAnchors.js';
 
 import './js/registerUser';
