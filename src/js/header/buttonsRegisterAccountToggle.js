2b8aa3675395983eef29b991a5d951229c86f715
abakarovmaks <abakarovmaks@gmail.com>
2020-12-18 12:33:35 +0200

    Добавляет в функцию перезагрузкк страницы после нажатия на кнопку выхода с аккаунта

--- src/js/header/buttonsRegisterAccountToggle.js
+++ src/js/header/buttonsRegisterAccountToggle.js
@@ -1,38 +1,3 @@
-// Мой кабинет, Регистрация, Выход
-// const myAccountBtn = document.querySelectorAll('[data-account-button]');
-// const registerBtn = document.querySelectorAll('[data-auth-button]');
-// const logOutBtn = document.querySelectorAll('[data-logout-button]');
-
-//Функции для смены кнопок авторизации и Мой кабинет
-
-//Отображает кнопку Мой кабинет
-// export function showMyAccountBtn() {
-//   registerBtn.forEach(e => {
-//     e.classList.add('is-logout');
-//   });
-
-//   myAccountBtn.forEach(e => {
-//     e.classList.add('is-auth');
-//   });
-
-//   logOutBtn.forEach(e => {
-//     e.classList.add('is-auth');
-//   });
-// }
-
-// //Отображает кнoпку регистрации
-// export function showRegisterBtn() {
-//   registerBtn.forEach(e => {
-//     e.classList.remove('is-logout');
-//   });
-//   myAccountBtn.forEach(e => {
-//     e.classList.remove('is-auth');
-//   });
-//   logOutBtn.forEach(e => {
-//     e.classList.remove('is-auth');
-//   });
-// }
-
 export function checkAuth() {
   // Мой кабинет, Регистрация, Выход
   const myAccountBtn = document.querySelector(
@@ -43,18 +8,19 @@ export function checkAuth() {
 
   logOutBtn.addEventListener('click', () => {
     localStorage.clear();
+    location.reload();
   });
 
   //Смена кнопок авторизации и Мой кабинет
   const token = localStorage.getItem('token');
   if (token === null) {
-    // console.log(token);
     myAccountBtn.classList.add('is-logout');
     logOutBtn.classList.add('is-logout');
   } else {
     myAccountBtn.classList.remove('is-logout');
     logOutBtn.classList.remove('is-logout');
     registerBtn.classList.add('is-logout');
+    // location.reload();
   }
 }
 
